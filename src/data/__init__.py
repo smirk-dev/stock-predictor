04a2b02f5fbd874c261b98a13719a538ad60e9f4
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-11-29 23:53:56 +0530

    hjb

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,8 +2,8 @@
 Data module for loading and managing stock data.
 """
 
-from .live_data_fetcher import LiveDataFetcher
+from .live_data_fetcher import LiveDataFetcher, POPULAR_US_STOCKS, POPULAR_INDIAN_STOCKS
 from .cache_manager import CacheManager
 from .data_service import DataService
 
-__all__ = ['LiveDataFetcher', 'CacheManager', 'DataService']
+__all__ = ['LiveDataFetcher', 'CacheManager', 'DataService', 'POPULAR_US_STOCKS', 'POPULAR_INDIAN_STOCKS']
